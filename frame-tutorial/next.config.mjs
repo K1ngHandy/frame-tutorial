a80d4c315a09895edecabdc216936403c63c11be
K1ngHandy <bolds-slicks0g@icloud.com>
2024-02-20 15:12:38 -0500

    next config update

--- frame-tutorial/next.config.mjs
+++ frame-tutorial/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/nativemuseum',
+                destination: 'https://nativemuseum.cloud',
+                permanent: false
+            },
+            {
+                source: '/pinatacloud',
+                destination: 'https://pinata.cloud/blog',
+                permanent: false
+            }
+        ]
+    }
+};
 
 export default nextConfig;
