adb6a670657c45e1e66c1d33a2a7a6488e62704a
K1ngHandy <bolds-slicks0g@icloud.com>
2024-02-24 14:31:34 -0500

    correct root layout typo in layout.tsx

--- frame-tutorial/app/layout.tsx
+++ frame-tutorial/app/layout.tsx
@@ -3,7 +3,7 @@ export const viewport = {
   initialScale: 1.0,
 };
 
-export default function Rootlayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <body>{children}</body>
