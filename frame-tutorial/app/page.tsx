af4b08f4afa6439efdff50581d6252c3bd14d802
K1ngHandy <bolds-slicks0g@icloud.com>
2024-02-24 14:29:18 -0500

    update openGraph title and url

--- frame-tutorial/app/page.tsx
+++ frame-tutorial/app/page.tsx
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
   title: 'Test Frame',
   description: 'A frame to test unique Frame building',
   openGraph: {
-    title: 'Test Frame',
+    title: 'Native Museum',
     description: 'A frame to test unique Frame building',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/`]
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmeqbjaHeUxrMDrqZGH81tm3aX29VZr5w9ZVzns6tPEJt7`]
   },
   other: {
     ...frameMetadata,
